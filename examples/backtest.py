3963fddec690270bc92fdceb280447f4929c9b84
Adam Rothman <adam@other.xyz>
2017-07-07 09:19:40 -0700

    Make examples/ scripts more script-like with flags and stuff

--- examples/backtest.py
+++ examples/backtest.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
+from argparse import ArgumentParser
 
 from moneybot.evaluate import evaluate
 from moneybot.fund import Fund
@@ -7,18 +9,40 @@ from moneybot.markets.adapters.backtest import BacktestMarketAdapter
 from moneybot.strategies.buffed_coin import BuffedCoinStrategy
 
 
-with open('config.json.example') as cfg_file:
-    config = json.load(cfg_file)
+def main(args):
+    with open(args.config) as cfg_file:
+        config = json.load(cfg_file)
 
-# Try BuyHoldStrategy or BuffedCoinStrategy too
-strat = Fund(BuffedCoinStrategy, BacktestMarketAdapter, config)
+    # Try BuyHoldStrategy or BuffedCoinStrategy too
+    strat = Fund(BuffedCoinStrategy, BacktestMarketAdapter, config)
 
-summary = evaluate(
-    strat,
-    '2017-01-01',
-    '2017-06-29',
-    duration_days=30,
-    window_distance_days=14,
-)
+    summary = evaluate(
+        strat,
+        '2017-01-01',
+        '2017-06-29',
+        duration_days=30,
+        window_distance_days=14,
+    )
 
-print(summary)
+    print(summary)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-c', '--config',
+        default='config.json.example',
+        type=str,
+        help='path to config file',
+    )
+    parser.add_argument(
+        '-l', '--log-level',
+        default='INFO',
+        type=str,
+        choices=['NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        help='Python logging level',
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level)
+    main(args)
--- examples/live_trading.py
+++ examples/live_trading.py
@@ -1,14 +1,52 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
+from argparse import ArgumentParser
 
 from moneybot.fund import Fund
 from moneybot.markets.adapters.live import LiveMarketAdapter
 from moneybot.strategies.buffed_coin import BuffedCoinStrategy
+from moneybot.strategies.buy_hold import BuyHoldStrategy
+from moneybot.strategies.peak_rider import PeakRiderStrategy
 
 
-with open('config.json') as cfg_file:
-    config = json.load(cfg_file)
+strategies = {
+    'buffed-coin': BuffedCoinStrategy,
+    'buy-hold': BuyHoldStrategy,
+    'peak-rider': PeakRiderStrategy,
+}
 
-# strat = BuffedCoinStrategy(LiveMarketAdapter, config)
-fund = Fund(BuffedCoinStrategy, LiveMarketAdapter, config)
-fund.run_live()
+
+def main(args):
+    with open(args.config) as cfg_file:
+        config = json.load(cfg_file)
+
+    fund = Fund(strategies[args.strategy], LiveMarketAdapter, config)
+    fund.run_live()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-c', '--config',
+        default='config.json.example',
+        type=str,
+        help='path to config file',
+    )
+    parser.add_argument(
+        '-l', '--log-level',
+        default='INFO',
+        type=str,
+        choices=['NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        help='Python logging level',
+    )
+    parser.add_argument(
+        '-s', '--strategy',
+        default='buffed_coin',
+        type=str,
+        choices=strategies.keys(),
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level)
+    main(args)
