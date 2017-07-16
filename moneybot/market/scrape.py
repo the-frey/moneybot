0960c125e1d310286d18c5af106420157a9ae365
elsehow <yes@cosmopol.is>
2017-07-16 14:37:16 -0700

    Scraper postgresified

--- moneybot/market/scrape.py
+++ moneybot/market/scrape.py
@@ -4,8 +4,10 @@ from datetime import datetime
 from logging import getLogger
 from typing import Optional
 from typing import Dict
+from typing import Iterable
+from typing import Tuple
 from funcy import compose
-from dateutil.parser import parse
+from funcy import partial
 
 from pandas import DataFrame
 from pandas import to_datetime
@@ -35,95 +37,93 @@ def market_cap(hist_ticker: Dict) -> Series:
     for key, vals in hist_ticker.items():
         if ts is None:
             ts = [to_datetime(t[0] * 1000000) for t in vals]
-            r[key] = [t[1] for t in vals]
+        r[key] = [t[1] for t in vals]
     return DataFrame(r, index=ts)
 
 
 coin_history = compose(market_cap, historical)
 
 
-def marshall(hist_df, key='price_btc'):
+def marshall(hist_df):
     btc_to_usd = hist_df['price_usd'] / hist_df['price_btc']
     # volume in BTC
+    # TODO is this correct? or is `'volume'` the quote volume?
     hist_df['volume'] = hist_df['volume_usd'] / btc_to_usd
     hist_df = hist_df.drop([
         'market_cap_by_available_supply',
         'volume_usd'
     ], axis=1)
-    hist_df = hist_df.rename(columns={key: 'weighted_average'})
+    hist_df['weighted_average'] = hist_df['price_usd']
+    hist_df['time'] = hist_df.index
+    hist_df['currency_pair'] = hist_df.apply(lambda x: 'USD_BTC', axis=1)
+    nothing_burger = lambda: hist_df.apply(lambda x: None, axis=1)
+    hist_df['open'] = nothing_burger()
+    hist_df['high'] = nothing_burger()
+    hist_df['low'] = nothing_burger()
+    hist_df['close'] = nothing_burger()
+    hist_df['quote_volume'] = nothing_burger()
     return hist_df
 
 
 def historical_prices_of(
-    btc_price_history: Series,
-    pair: str,
-    period: int = 900,
-    start: Optional[float] = None,
-    end: Optional[float] = None
-) -> Series:
+        polo: Poloniex,
+        btc_price_history: Series,
+        pair: str,
+        period: int = 900,
+        start: Optional[float] = None,
+        end: Optional[float] = None
+        ) -> Iterable[Series]:
     '''
     Returns a series of time-indexed prices.
     `pair` is of the form e.g. 'BTC_ETH',
     `period` is an integer number of seconds,
     either 300, 900, 1800, 7200, 14400, or 86400.
+
+    We do some data marshalling in this method as well,
+    to turn API results into stuff amenable for our Postgres DB.
     '''
     def contemporary_usd_price(row: Series) -> float:
         contemporary_btc_price = btc_price_history['price_usd'].asof(row.name)
-        return row['weighted_average'] * contemporary_btc_price
-
+        return row['weightedAverage'] * contemporary_btc_price
+    # Scraping
     now = time.time()
     start = start or now - YEAR_IN_SECS
     end = end or now
-
     ex_trades = polo.returnChartData(pair, period, start, end)
+    # Data marshalling
     ts_df = DataFrame(ex_trades, dtype=float)
-    ts_df.index = [datetime.fromtimestamp(t) for t in ts_df['date']]
-    ts_df = ts_df.drop(['date'], axis=1)
-    return ts_df.apply(contemporary_usd_price, axis=1)
-    # ts_df['price_usd'] = ts_df.apply(contemporary_usd_price, axis=1)
-    # return ts_df
-
-# def insert (cursor, measurement):
-#     measurement['time'] = parse(measurement['time'])
-#     return cursor.execute(
-#          """INSERT INTO scraped_chart (time, currency_pair, high, low, price_usd, quote_volume, volume, weighted_average)
-#         VALUES (%(time)s, %(currencyPair)s, %(high)s, %(low)s, %(price_usd)s, %(quoteVolume)s, %(volume)s, %(weightedAverage)s);""",
-#         measurement)
-# def scraped_chart(currency_pair: str, row: Series) -> Dict:
-#     return {
-#         'measurement': 'scraped_chart',
-#         'tags': {
-#             'currency_pair': currency_pair,
-#         },
-#         'time': format_time(row.name),
-#         'fields': row.to_dict(),
-#     }
-
-# TODO This should be postgresified
-def scrape_since_last_reading():
-
-    client = Postgres.get_client()
-    polo = Poloniex.get_client()
-    btc_price_hist = coin_history('bitcoin')
-
-
-
-
-
+    ts_df['time'] = [datetime.fromtimestamp(t) for t in ts_df['date']]
+    ts_df.index = ts_df['time']
+    ts_df['price_usd'] = ts_df.apply(contemporary_usd_price, axis=1)
+    ts_df['currency_pair'] = ts_df.apply(lambda x: pair, axis=1)
+    ts_df = ts_df.rename(index=str, columns={
+        'quoteVolume': 'quote_volume',
+        'weightedAverage': 'weighted_average',
+    })
     for _, row in ts_df.iterrows():
         # chart = scraped_chart(pair, row)
         # for some reason, when there's no chart data to report,
         # the API will give us some reading with all 0s.
-        if row['volume'] == 0 and row['weightedAverage'] == 0:
+        if row['volume'] == 0 and row['weighted_average'] == 0:
             # we will just ignore these
             pass
         else:
-            yield insert(cursor, pair, row)
+            yield row
 
+def insert (cursor, row):
+    return cursor.execute(
+         """INSERT INTO scraped_chart (time, currency_pair, high, low, price_usd, quote_volume, volume, weighted_average)
+        VALUES (%(time)s, %(currency_pair)s, %(high)s, %(low)s, %(price_usd)s, %(quote_volume)s, %(volume)s, %(weighted_average)s);""",
+        row.to_dict())
 
-    # Get the last time we fetched some data,
-    # looking at the most recent result in the db
+def scrape_since_last_reading():
+
+    # postgres client
+    client = Postgres.get_client()
     cursor = client.cursor()
+    inserter = partial(insert, cursor)
+    # get the last time we fetched some data,
+    # looking at the most recent result in the db
     query = ' '.join([
         'select time from scraped_chart',
         'order by time desc',
@@ -131,29 +131,32 @@ def scrape_since_last_reading():
     ])
     cursor.execute(query)
     latest_fetch_time = cursor.fetchone()[0]
-    # convert from string to unix time
     latest_fetch_unix = time.mktime(latest_fetch_time.timetuple())
 
+    # now get USD_BTC history
+    btc_price_hist = coin_history('bitcoin')
+    # and write that history to DB,
+    btc_rows = marshall(btc_price_hist)
+    # since latest fetch time.
+    recent_btc = btc_rows[btc_rows['time'] > latest_fetch_time]
+    [inserter(row) for _, row in recent_btc.iterrows()]
+    client.commit()
+    logger.debug('Scraped USD_BTC')
+
+    # now, a poloniex client
+    polo = Poloniex.get_client()
+    # and a method for grabbing historical prices
+    grab_historical_prices = partial(historical_prices_of, polo, btc_price_hist)
     # for each market,
     for market in polo.returnTicker():
-        # fetch all the chart data
-        # since that last fetch.
-        # try:
-        generator = historical_prices_of(
+        # fetch all the chart data since last fetch
+        generator = grab_historical_prices(
             market,
             start=latest_fetch_unix,
             end=time.time(),
         )
-        # TODO POSTGRES
-        client.write_points(generator)
+        list(map(inserter, generator))
+        client.commit()
         logger.debug(f'Scraped {market}')
-        # except:
-        #     logger.error(f'error scraping market: {market}')
 
-
-    # Finally, write USD_BTC history to the client as well
-    btc_rows = marshall(btc_price_hist, key='price_usd')
-    # TODO POSTGRES
-    client.write_points(scraped_chart('USD_BTC', row)
-                        for _, row in btc_rows.iterrows())
-    logger.debug('Scraped USD_BTC')
+    cursor.close()
